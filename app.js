0f6025e98012ba95f76ab45e6a099826d7e26434
PatrickPatch <patrickclarke1983@gmail.com>
2023-09-14 22:07:27 +0100

    js works on loading with marquee and button is centered

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
 console.log ("hello world!");
 // Hype Measurement
 
-function greet() { 
-    let visitor = prompt("What is your name friend?");
-}
+// function greet() { 
+//     let visitor = prompt("What is your name friend?");
+// }
+
+
 
 function Excited() {
-    let hype = prompt("On a scale of ten, how excited are you to see Gertrude the elephant?");
-}
-
-function refreshMarqee() {  
-    document.getElementById("h3").style.backgroundColor = "green";
-}
-
-
-let visitor = prompt("What is your name friend?")
-
-let hype = prompt("On a scale of ten, how excited are you to see Gertrude the elephant?")
-// console.log(visitor);
-// console.log(hype);
-
-
-
-if (hype < 5) {
-    message = ", you have upset Gertude with your indifference.🐘🐘🐘🐘🐘";
-}
-else if (hype > 6) {
-    message = " is awesome! Yeaaaah! Elephants! YEAAAH!! STOMP STOMP!🐘🐘🐘🐘🐘";
-}
-else {
-    message = " try again, friend🐘🐘🐘🐘🐘";
-}
-
-// reHyped.addEventListener("click", hype() 
+    let hype = prompt("On a scale of ten, how excited are you to see Gertrude the elephant?"); 
+    if (hype < 5) {
+        message = ", you have upset Gertude with your indifference.🐘🐘🐘🐘🐘";
+    }
+    else if (hype > 6) {
+        message = " is awesome! Yeaaaah! Elephants! YEAAAH!! STOMP STOMP!🐘🐘🐘🐘🐘";
+    }
+    else {
+        message = " try again, friend🐘🐘🐘🐘🐘";
+    }
+    {
+        document.getElementById('scroller').innerHTML = (visitor  +  message);  
+    }
     
-//concatenation
-// document.write(visitor  +  message)
+}
+let visitor = prompt("What is your name friend?")
+// let hype = prompt("On a scale of ten, how excited are you to see Gertrude the elephant?") 
+Excited()
+
