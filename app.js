39260c16c787266a0a1cad13fb0b0ec51a01c32a
PatrickPatch <patrickclarke1983@gmail.com>
2023-09-14 16:52:20 +0100

    added marqee responsive to java elements

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 console.log ("hello world!");
 // Hype Measurement
+
+function greet() { 
+    let visitor = prompt("What is your name friend?");
+}
+
+function Excited() {
+    let hype = prompt("On a scale of ten, how excited are you to see Gertrude the elephant?");
+}
+
+function refreshMarqee() {  
+    document.getElementById("h3").style.backgroundColor = "green";
+}
+
+
 let visitor = prompt("What is your name friend?")
 
 let hype = prompt("On a scale of ten, how excited are you to see Gertrude the elephant?")
-console.log(visitor);
-console.log(hype);
+// console.log(visitor);
+// console.log(hype);
+
+
 
 if (hype < 5) {
-    message = ", you have upset Gertude with your indifference.";
+    message = ", you have upset Gertude with your indifference.🐘🐘🐘🐘🐘";
 }
 else if (hype > 6) {
-    message = " is awesome! Yeaaaah! Elephants! YEAAAH!! STOMP STOMP!";
+    message = " is awesome! Yeaaaah! Elephants! YEAAAH!! STOMP STOMP!🐘🐘🐘🐘🐘";
 }
 else {
-    message = " had one job. Try again, maybe Gertrude didn't see";
+    message = " try again, friend🐘🐘🐘🐘🐘";
 }
 
+// reHyped.addEventListener("click", hype() 
+    
 //concatenation
-console.log(visitor  +  message)
+// document.write(visitor  +  message)
