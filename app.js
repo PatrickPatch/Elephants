d491986f70d1d9a26f6e8c1381520a93c62c27cf
PatrickPatch <patrickclarke1983@gmail.com>
2023-09-15 11:53:08 +0100

    added java website counter with images, also added additional elseif for the enthusiasm counter for numbers higher than ten.

--- app.js
+++ app.js
@@ -12,11 +12,15 @@ function Excited() {
     if (hype < 5) {
         message = ", you have upset Gertude with your indifference.🐘🐘🐘🐘🐘";
     }
+    else if (hype > 10) {
+        message = " has too much excitement friend, take some deep breaths and drink some water 🐘🐘🐘🐘🐘";
+    }
     else if (hype > 6) {
         message = " is awesome! Yeaaaah! Elephants! YEAAAH!! STOMP STOMP!🐘🐘🐘🐘🐘";
     }
     else {
-        message = " try again, friend🐘🐘🐘🐘🐘";
+        message = " try again, friend🐘🐘🐘🐘🐘"
+        Excited()
     }
     {
         document.getElementById('scroller').innerHTML = (visitor  +  message);  
@@ -27,3 +31,12 @@ let visitor = prompt("What is your name friend?")
 // let hype = prompt("On a scale of ten, how excited are you to see Gertrude the elephant?") 
 Excited()
 
+let rating = prompt('rate my site between 1-5', '5');
+
+for (let i = 0; i < rating; i++) {
+    document.write("<img src='./angry elephant.jpg'/>")
+    document.write(i+1)
+}
+
+
+        // message = " try again, friend🐘🐘🐘🐘🐘";
